757ee68d48a72864d00ca14c5862419f33c8fb3e
Henrique <suporte@pcmsolucoes.com.br>
2020-05-30 19:22:35 -0300

    Final aula 6.

--- src/Controller/Exclusao.php
+++ src/Controller/Exclusao.php
@@ -3,10 +3,12 @@
 namespace Alura\Cursos\Controller;
 
 use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Helper\FlashMessageTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
 
 class Exclusao implements InterfaceControladorRequisicao
 {
+    use FlashMessageTrait;
     /**
      * @var \Doctrine\ORM\EntityManagerInterface
      */
@@ -28,6 +30,7 @@ class Exclusao implements InterfaceControladorRequisicao
 
 
         if(is_null($id) || $id === false) {
+            $this->defineMensagem('danger', 'Curso inexistente');
             header('Location: /listar-cursos');
             return;
         }
@@ -35,6 +38,8 @@ class Exclusao implements InterfaceControladorRequisicao
         $curso = $this->entityManager->getReference(Curso::class, $id);
         $this->entityManager->remove($curso);
         $this->entityManager->flush();
+
+        $this->defineMensagem('success', 'Curso excluído com sucesso');
         header('Location: /listar-cursos');
     }
 }
--- src/Controller/RealizarLogin.php
+++ src/Controller/RealizarLogin.php
@@ -3,11 +3,13 @@
 namespace Alura\Cursos\Controller;
 
 use Alura\Cursos\Entity\Usuario;
+use Alura\Cursos\Helper\FlashMessageTrait;
 use Alura\Cursos\Infra\EntityManagerCreator;
 
 
 class RealizarLogin implements InterfaceControladorRequisicao
 {
+    use FlashMessageTrait;
     /**
      * @var \Doctrine\Common\Persistence\ObjectRepository
      */
@@ -28,7 +30,8 @@ class RealizarLogin implements InterfaceControladorRequisicao
         );
 
         if(is_null($email) || $email === false) {
-            echo "O e-mail digitado não é um e-mail válido";
+            $this->defineMensagem('danger', 'O e-mail digitado não é um e-mail válido');
+            header('Location: /login');
             return;
         }
 
@@ -41,9 +44,12 @@ class RealizarLogin implements InterfaceControladorRequisicao
         $usuario = $this->repositorioDeUsuarios->findOneBy(['email' => $email]);
 
         if(is_null($usuario) || !$usuario->senhaEstaCorreta($senha)) {
-            echo "E-mail ou senha inválidos";
+            $this->defineMensagem('danger', 'E-mail ou senha inválidos');
+            header('Location: /login');
             return;
         }
+
+        $_SESSION['logado'] = true;
 
         header('Location: /listar-cursos');
     }
