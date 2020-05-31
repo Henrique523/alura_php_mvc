9f833e0ed4a456cbdeb17f50ea9a55527e68a0dd
Henrique <suporte@pcmsolucoes.com.br>
2020-05-31 10:33:24 -0300

    Final aula 7.

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-use Alura\Cursos\Controller\{Deslogar,
+use Alura\Cursos\Controller\{CursosEmJson,
+    CursosEmXml,
+    Deslogar,
     Exclusao,
     FormularioEdicao,
     FormularioInsercao,
@@ -18,4 +20,6 @@ return [
     '/login' => FormularioLogin::class,
     '/realiza-login' => RealizarLogin::class,
     '/logout' => Deslogar::class,
+    '/buscarCursosEmJson' => CursosEmJson::class,
+    '/buscarCursosEmXml' => CursosEmXml::class,
 ];
